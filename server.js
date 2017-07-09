ee1732ab4ba6b476575765664ed6d66febe394ec
Adrien Leravat <Pixep@users.noreply.github.com>
2017-07-09 19:28:27 +0200

    Added a second analysis server ws on a different port

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
+var controlServer = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
 });
 
-server.listen(5880, function() {
-    console.log((new Date()) + ' Server is listening on port 5880');
+var analysisServer = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
 });
 
-wsServer = new WebSocketServer({
-    httpServer: server,
+controlServer.listen(5880, function() {
+    console.log((new Date()) + ' Control server is listening on port 5880');
+});
+analysisServer.listen(5881, function() {
+    console.log((new Date()) + ' Analysis server is listening on port 5881');
+});
+
+wsControlServer = new WebSocketServer({
+    httpServer: controlServer,
+    autoAcceptConnections: false
+});
+wsAnalysisServer = new WebSocketServer({
+    httpServer: analysisServer,
     autoAcceptConnections: false
 });
 
@@ -21,7 +34,7 @@ function originIsAllowed(origin) {
   return true;
 }
 
-wsServer.on('request', function(request) {
+wsControlServer.on('request', function(request) {
     if (!originIsAllowed(request.origin)) {
       // Make sure we only accept requests from an allowed origin
       request.reject();
@@ -33,15 +46,13 @@ wsServer.on('request', function(request) {
     var connection = request.accept(null, request.origin);
     console.log((new Date()) + ' Connection accepted from ' + request.origin);
 
-    // Echo text received
+    // Handle reception
     connection.on('message', function(message) {
         if (message.type !== 'utf8') {
             return;
         }
 
-        console.log('Received Message: ' + message.utf8Data);
-
-        // Process message
+        console.log('Control message received: ' + message.utf8Data);
     });
 
     // Close handler
@@ -63,4 +74,31 @@ wsServer.on('request', function(request) {
     }
 
     connection.controlTimer = setInterval(sendControl, 1500);
+});
+
+wsAnalysisServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+
+    // Accept connection
+    var connection = request.accept(null, request.origin);
+    console.log((new Date()) + ' Connection accepted from ' + request.origin);
+
+    // Handle reception
+    connection.on('message', function(message) {
+        if (message.type !== 'utf8') {
+            return;
+        }
+
+        console.log('Analysis message received: ' + message.utf8Data);
+    });
+
+    // Close handler
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
 });
