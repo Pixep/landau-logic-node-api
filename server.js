b62c03005a73beaf6ccd6021054753fe83a74f7f
Adrien Leravat <Pixep@users.noreply.github.com>
2017-08-27 17:51:36 +0200

    Fixed indentation

--- server.js
+++ server.js
@@ -2,127 +2,127 @@ var WebSocketServer = require('websocket').server;
 var http = require('http');
 
 var controlServer = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
+  console.log((new Date()) + ' Received request for ' + request.url);
+  response.writeHead(404);
+  response.end();
 });
 
 var analysisServer = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
+  console.log((new Date()) + ' Received request for ' + request.url);
+  response.writeHead(404);
+  response.end();
 });
 
 controlServer.listen(5880, function() {
-    console.log((new Date()) + ' Control server is listening on port 5880');
+  console.log((new Date()) + ' Control server is listening on port 5880');
 });
 analysisServer.listen(5881, function() {
-    console.log((new Date()) + ' Analysis server is listening on port 5881');
+  console.log((new Date()) + ' Analysis server is listening on port 5881');
 });
 
 wsControlServer = new WebSocketServer({
-    httpServer: controlServer,
-    autoAcceptConnections: false
+  httpServer: controlServer,
+  autoAcceptConnections: false
 });
 wsAnalysisServer = new WebSocketServer({
-    httpServer: analysisServer,
-    autoAcceptConnections: false
+  httpServer: analysisServer,
+  autoAcceptConnections: false
 });
 
 function originIsAllowed(origin) {
-    // put logic here to detect whether the specified origin is allowed.
-    return true;
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
 }
 
 var commandWs;
 var analysisWs;
 
 wsControlServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection to control server from origin ' + request.origin + ' rejected.');
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject();
+    console.log((new Date()) + ' Connection to control server from origin ' + request.origin + ' rejected.');
+    return;
+  }
+
+  // Accept connection
+  var connection = request.accept(null, request.origin);
+  console.log((new Date()) + ' Connection to control server accepted from ' + request.origin);
+
+  commandWs = connection;
+
+  // Handle reception
+  connection.on('message', function(message) {
+    if (message.type !== 'utf8') {
       return;
     }
 
-    // Accept connection
-    var connection = request.accept(null, request.origin);
-    console.log((new Date()) + ' Connection to control server accepted from ' + request.origin);
+    console.log('Control message received: ' + message.utf8Data);
+  });
 
-    commandWs = connection;
+  // Close handler
+  connection.on('close', function(reasonCode, description) {
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    clearInterval(connection.controlTimer);
+  });
 
-    // Handle reception
-    connection.on('message', function(message) {
-        if (message.type !== 'utf8') {
-            return;
-        }
-
-        console.log('Control message received: ' + message.utf8Data);
-    });
-
-    // Close handler
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-        clearInterval(connection.controlTimer);
-    });
-
-    // Send control periodically
-    function sendControl() {
-        var data = {
-            steering: 0,
-            acceleration: -0.5,
-            brake: 0.5,
-            handBrake: 0
-        }
-
-        //connection.sendUTF(JSON.stringify(data));
-    }
-
-    connection.controlTimer = setInterval(sendControl, 1500);
+  // Send control periodically
+  //connection.controlTimer = setInterval(sendControl, 1500);
 });
 
+function sendControl() {
+  var data = {
+    steering: 0,
+    acceleration: -0.5,
+    brake: 0.5,
+    handBrake: 0
+  }
+
+  connection.sendUTF(JSON.stringify(data));
+}
+
 wsAnalysisServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-        // Make sure we only accept requests from an allowed origin
-        request.reject();
-        console.log((new Date()) + ' Connection to analysis server from origin ' + request.origin + ' rejected.');
-        return;
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject();
+    console.log((new Date()) + ' Connection to analysis server from origin ' + request.origin + ' rejected.');
+    return;
+  }
+
+  // Accept connection
+  var connection = request.accept(null, request.origin);
+  console.log((new Date()) + ' Connection to analysis server accepted from ' + request.origin);
+
+  analysisWs = connection;
+
+  // Handle reception
+  connection.on('message', function(message) {
+    if (message.type !== 'utf8') {
+      return;
     }
 
-    // Accept connection
-    var connection = request.accept(null, request.origin);
-    console.log((new Date()) + ' Connection to analysis server accepted from ' + request.origin);
+    console.log('Analysis message received: ' + message.utf8Data);
+    if (!commandWs)
+      return;
 
-    analysisWs = connection;
+    var sensors = JSON.parse(message.utf8Data);
+    var goBack = false;
 
-    // Handle reception
-    connection.on('message', function(message) {
-        if (message.type !== 'utf8') {
-            return;
-        }
+    if (!sensors.value)
+      return;
 
-        console.log('Analysis message received: ' + message.utf8Data);
-        if (!commandWs)
-            return;
+    if (sensors.value[0] == 0)
+      goBack = false;
+    else
+      goBack = true;
 
-        var sensors = JSON.parse(message.utf8Data);
-        var goBack = false;
+    controlVehicle(goBack ? 0 : 0.3, goBack ? -0.6 : 0, goBack ? 0.5 : 0);
+  });
 
-        if (!sensors.value)
-          return;
-
-        if (sensors.value[0] == 0)
-            goBack = false;
-        else
-            goBack = true;
-
-        controlVehicle(goBack ? 0 : 0.3, goBack ? -0.6 : 0, goBack ? 0.5 : 0);
-    });
-
-    // Close handler
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
+  // Close handler
+  connection.on('close', function(reasonCode, description) {
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
 });
 
 function controlVehicle(_acceleration, _steering, _brake) {
